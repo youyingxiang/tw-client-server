f00002656ec696116465d4f3586ada7738bce091
Yzzz <383045501@qq.com>
2019-06-11 10:48:05 +0800

    '061110'

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -40,10 +40,10 @@
                             <li>
                                 <div class="pt_left">
                                     <h4>{{$vo['title']}}</h4>
-                                    <p>期限:{!! $vo['term'] !!}</p>
+                                    <p style="color: #fff">期限:{!! $vo['term'] !!}</p>
                                 </div>
                                 <div class="pt_right">
-                                    <div class="pt_ico"><i class="img_1"></i><p>活动设置</p></div>
+                                    <div class="pt_ico"><a href="{!!route('tw.activity.edit',$vo['id'])!!}"><i class="img_1"></i><p>活动设置</p></a></div>
                                     <div class="pt_ico"><i class="img_2"></i><p>大屏幕</p></div>
                                     <div class="pt_ico"><i class="img_3"></i><p>评委设置</p></div>
                                     <div class="pt_ico"><i class="img_4"></i><p>选手设置</p></div>
@@ -54,43 +54,6 @@
                         </ul>
                     </div>
 
-
-
-                    {{--<div class="box-body table-responsive" id="putong">--}}
-                        {{--<table class="table table-bordered table-hover table-sort">--}}
-                            {{--<tr>--}}
-                                {{--<th width="35"><input type="checkbox" class="minimal checkbox-toggle"></th>--}}
-                                {{--<th>ID:{!! table_sort('id') !!}</th>--}}
-                                {{--<th>活动名称</th>--}}
-                                {{--<th>logo</th>--}}
-                                {{--<th>评分方式</th>--}}
-                                {{--<th>背景图片</th>--}}
-                                {{--<th>期限</th>--}}
-                                {{--<th>操作</th>--}}
-                            {{--</tr>--}}
-                            {{--@foreach($aData as $vo)--}}
-                                {{--<tr>--}}
-                                    {{--<td style="vertical-align:middle"><input type="checkbox" name="id[]"--}}
-                                                                             {{--value="{{$vo['id']}}" class="minimal"></td>--}}
-                                    {{--<td style="vertical-align:middle">{{$vo['id']}}</td>--}}
-                                    {{--<td style="vertical-align:middle"><span class="editable" data-pk="{{$vo['id']}}}"--}}
-                                                                            {{--data-name="title"--}}
-                                                                            {{--data-url="">{{$vo['title']}}</span></td>--}}
-                                    {{--<td style="vertical-align:middle"><img src="{{$vo['logo']}}"--}}
-                                                                           {{--style="width:40px;border-radius:40%;"/></td>--}}
-                                    {{--<td style="vertical-align:middle">{{get_score_type($vo['score_type'])}}</td>--}}
-                                    {{--<td style="vertical-align:middle"><img src="{{$vo['banner']}}"--}}
-                                                                           {{--style="width:40px;border-radius:40%;"/></td>--}}
-                                    {{--<td style="vertical-align:middle">{!! $vo['term'] !!}</td>--}}
-                                    {{--<td style="vertical-align:middle">--}}
-                                        {{--{!! $vo['jump'] !!}--}}
-                                        {{--{!! button(route('tw.activity.edit',$vo['id']),'edit') !!}--}}
-                                        {{--{!! button(route('tw.activity.destroy',$vo['id']),'delete',$vo['id']) !!}--}}
-                                    {{--</td>--}}
-                                {{--</tr>--}}
-                            {{--@endforeach--}}
-                        {{--</table>--}}
-                    {{--</div>--}}
                     {{--高级--}}
                     <div class="actvity_pt" id="gaoji" style="display: none">
                         <ul>
